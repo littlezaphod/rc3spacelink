7ce316d3a3357cd0f7780928e28f9f6870648791
Patric Nolle <patric.nolle@gmail.com>
2020-12-20 15:52:52 +0100

    First commit First commit# with '#' will be ignored, and an empty message aborts the commit.

---
+++ tiles/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="grass" tilewidth="32" tileheight="32" tilecount="70" columns="10">
+ <image source="grass.png" width="320" height="224"/>
+</tileset>
