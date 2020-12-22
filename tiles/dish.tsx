3b9eaade8d8788bc211520b31153d622617a8960
Patric Nolle <patric.nolle@gmail.com>
2020-12-22 16:09:27 +0100

    First functional version

---
+++ tiles/dish.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="dish" tilewidth="32" tileheight="32" margin="20" tilecount="20" columns="5">
+ <image source="custom/dish.png" width="180" height="152"/>
+</tileset>
---
+++ tiles/mapUtilities.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="mapUtilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
+ <image source="tiles_png/community/cert/imgs/tilesheets/mapUtilities.png" width="288" height="32"/>
+</tileset>
