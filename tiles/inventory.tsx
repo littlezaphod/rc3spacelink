5842fc11d311eea6a311a385fb4edc02a280b40e
Patric Nolle <patric.nolle@gmail.com>
2020-12-25 14:37:15 +0100

    update

---
+++ tiles/inventory.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="thumb-0a7e1f99cff200bbb83bc5afeb9ea127608cff97-320x240" tilewidth="32" tileheight="32" tilecount="70" columns="10">
+ <image source="tiles_png/inventory/thumb-0a7e1f99cff200bbb83bc5afeb9ea127608cff97-320x240.jpg" width="320" height="240"/>
+</tileset>
